5d9d50e8c9b38c7bdddbdfddfe30c6cdc77b8826
Yayo Del Rey <yayodelrey@MacBook-Pro-de-Andres.local>
2019-09-24 17:46:35 -0500

    Done :octocat:

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Layout } from 'antd'
+
+const { Footer } = Layout
+
+function FooterItem() {
+  return (
+    <Footer style={{ textAlign: 'center' }}>Katy Perry Page created by: Elisa & Andres ️</Footer>
+  )
+}
+
+export default FooterItem
